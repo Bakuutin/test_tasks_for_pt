7a91a19672432f0546e70eae6290da81db9dde32
Igor Bakutin <igorbakutin@gmail.com>
2017-01-31 20:10:52 +0300

    First task

---
+++ task1.py
@@ -0,0 +1,24 @@
+def my_code(data, offset=0, offset_string='  '):
+    for key, value in data.items():
+        print(offset_string * offset, key, ':', sep='')
+
+        if isinstance(value, dict):
+            my_code(value, offset + 1, offset_string)
+        else:
+            print(offset_string * (offset + 1), value, sep='')
+
+
+if __name__ == '__main__':
+    my_code({
+        'first': 'first_value',
+        'second': 'second_value',
+    })
+
+    print()
+
+    my_code({
+        '1': {
+            'child': '1/child/value'
+        },
+        '2': '2/value'
+    })
